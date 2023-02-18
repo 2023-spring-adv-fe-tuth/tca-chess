c37560380fe7a0fdb5e5f9b9d5bb66456b8d6f2a
Heidi Kuhnau <95493237+hkuhnau@users.noreply.github.com>
2023-02-17 21:19:27 -0300

    Router, Routes, and Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import './styles.css';
-//import GameinPlay from './GameinPlay';
+import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-
+import GameinPlay from './GameinPlay';
 
 function App() {
   return (
-    <Home></Home>
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/gameinplay">Play Game</Link>
+
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+
+      <Routes>
+        <Route path="/gameinplay" element={<GameinPlay/>}></Route>
+      </Routes>
+
+    </Router>
   );
   
 }
